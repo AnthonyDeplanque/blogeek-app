d7e5baa09b8e341c3bfeb92a697ebf7cced42d99
anthony Deplanque <anthony.deplanque3184@gmail.com>
2021-10-03 15:56:28 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
+interface props { };
+
+const App: React.FC<props> = () => {
+  return (
+    <Box><Typography>Hello World</Typography></Box>
+  );
+}
+
+export default App;
